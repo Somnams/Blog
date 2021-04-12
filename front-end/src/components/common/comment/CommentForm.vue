08abcdda2320263ed620ee0f150467d91c09a438
Somnams <3244345132@qq.com>
2021-04-12 19:21:06 +0800

    🌈 Add CommentForm.vue

---
+++ front-end/src/components/common/comment/CommentForm.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <img src="" alt="">Comments Area
+    <form>
+      <textarea v-model="formData" cols="30" rows="10" placeholder="Please input your comment."></textarea>
+      <button type="submit" :disabled="disabled">submit</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CommentForm.vue',
+  data() {
+    return {
+      formData: '',
+      disabled: true
+    }
+  },
+  methods: {
+    onSubmit() {
+      if (!this.formData) {
+        this.disabled = true;
+        return false;
+      }
+      this.disabled = false;
+      console.log(this.formData);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
