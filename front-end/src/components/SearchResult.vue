b45d2166337626e09cf64d3602087e28648a4e35
Somnams <3244345132@qq.com>
2021-04-15 18:37:52 +0800

    💡 Fixed SearchResult.vue

--- front-end/src/components/SearchResult.vue
+++ front-end/src/components/SearchResult.vue
@@ -5,52 +5,23 @@
       <header>
         <nav-header />
       </header>
-      <div class="main-content">
+      <div class="main-content" v-if="posts">
         <h3>
-          All posts <small v-if="posts">({{ posts._meta.total_items }} / {{ posts._meta.total_pages }})</small>
+          All posts ({{ posts._meta.total_items }} / {{ posts._meta.total_pages }})
         </h3>
-        <div v-if="posts">
-          <div
-            v-for="(post, index) in posts.items"
-            :key="index"
-          >
-            <div class="result">
-              <div class="res-title">
-                <router-link :to="{ name: 'Post', params: { id: post.id } }">
-                  <vue-markdown :source="post.title" />
-                </router-link>
-              </div>
-              <div class="res-body">
-                <vue-markdown :source="post.summary" />
-              </div>
-              <ul class="res-other">
-                <li>
-                  <img
-                    :src="imageDatas.iconzhucetouxiang"
-                    class="icon-img"
-                  >
-                  <router-link :to="{ path: `/user/${post.author.id}` }">
-                    {{ post.author.username }}
-                  </router-link>
-                </li>
-                <li>
-                  <img
-                    :src="imageDatas.shijian"
-                    class="icon-img"
-                  ><span class="icon-font">{{
-                    $moment(post.timestamp).format('YYYY/MM/DD HH:mm:ss')
-                  }}</span>
-                </li>
-                <li>
-                  <img
-                    :src="imageDatas.chakancishu"
-                    class="icon-img"
-                  >{{ post.views }}
-                </li>
-              </ul>
+        <div v-for="(post, index) in posts.items" :key="index">
+          <div class="result-post">
+            <router-link :to="{name: 'Post', params: {id: post.id}}">
+              <markdown-preview :initialValue="post.summary" theme="dark" />
+            </router-link>
+            <div class="result-post-footer">
+              <img :src="imageData.iconzhucetouxiang" alt="author" class="icon-img">{{post.author.username}}
+              <img :src="imageData.shijian" alt="timestamp" class="icon-img">{{post.timestamp}}
+              <img :src="imageData.chakancishu" alt="view" class="icon-img">{{post.views}}
             </div>
           </div>
         </div>
+        <div>
         <confirm ref="confirm" />
         <div v-if="posts && posts._meta.total_pages > 1">
           <pagination
@@ -62,6 +33,7 @@
       </div>
     </div>
   </div>
+  </div>
 </template>
 
 <script>
@@ -69,7 +41,7 @@ import NavHeader from './common/nav/NavHeader';
 import NavBar from './common/nav/NavBar';
 import Pagination from './common/pagination/Pagination';
 import store from '@/store/store';
-import VueMarkdown from 'vue-markdown';
+import {MarkdownPreview} from 'vue-meditor';
 import { confirmMiXin } from '@/common/mixin';
 
 export default {
@@ -77,14 +49,13 @@ export default {
   components: {
     NavHeader,
     NavBar,
-    VueMarkdown,
+    MarkdownPreview,
     Pagination,
   },
   mixins: [confirmMiXin],
   data() {
     return {
       sharedState: store.state,
-      imageDatas: store.state.imageData,
       posts: '',
       postForm: {
         title: '',
@@ -104,106 +75,34 @@ export default {
       },
     };
   },
+  computed: {
+    imageData() {
+      return store.state.imageData;
+    }
+  },
   created() {
     this.getSearchResult();
   },
   methods: {
     getSearchResult() {
-      const { q } = this.$route.query;
-      let page = 1;
-      // eslint-disable-next-line camelcase
-      let per_page = 10;
-      let path;
-      if (typeof this.$route.query.page !== 'undefined') {
-        page = this.$route.query.page;
-      }
-      if (typeof this.$route.query.per_page !== 'undefined') {
-        // eslint-disable-next-line camelcase
-        per_page = this.$route.query.per_page;
-      }
-      if (typeof q !== 'undefined') {
-        // eslint-disable-next-line camelcase
-        path = `/search/?q=${q}&page=${page}&per_page=${per_page}`;
-      } else {
-        // eslint-disable-next-line camelcase
-        path = `/search/?page=${page}&per_page=${per_page}`;
-      }
-      this.$axios
-        .get(path)
-        .then(res => {
-          this.posts = res.data.data;
-          if (res.data.data._meta.total_items > 0) {
-            this.$toasted.success(res.data.message, { icon: 'fingerprint' });
-          } else {
-            this.$toasted.error('Can not find any result', { icon: 'fingerprint' });
+      const {q} = this.$route.query;
+      const page = typeof this.$route.query.page === 'undefined' ? 1 : this.$route.query.page;
+      const perPage = typeof this.$route.query.per_page === 'undefined' ? 10 : this.$route.query.per_page;
+      const path = typeof  q === 'undefined' ?
+        `/search/?page=${page}&per_page=${perPage}`
+        :
+        `/search/?q=${q}&page=${page}&per_page=${perPage}`;
+      this.$axios.get(path)
+        .then(({data}) => {
+          if (data.data._meta.total_items > 0) {
+            this.posts = data.data;
+            this.$toasted.success(data.message);
           }
         })
-        .catch(err => {
-          console.log(err.response.data);
-          this.posts = '';
-          this.$toasted.error(err.response.data.message, { icon: 'fingerrpint' });
-        });
-    },
-    // onEditPost (post) {
-    //   this.editForm = Object.assign({}, post)
-    // },
-    // onSubmitUpdatePost () {
-    //   this.editForm.errors = 0
-    //   if (!this.editPostForm.title) {
-    //     this.editForm.errors++
-    //     this.editForm.titleError = 'Title is required.'
-    //   } else {
-    //     this.editForm.titleError = null
-    //   }
-    //   if (!this.editForm.body) {
-    //     this.editForm.errors++
-    //     this.editForm.bodyError = 'Body is required.'
-    //   } else {
-    //     this.editForm.bodyError = null
-    //   }
-    //   if (this.editForm.errors > 0) {
-    //     return false
-    //   }
-    //   const path = `/posts/${this.editForm.id}`
-    //   const payload = {
-    //     title: this.editForm.title,
-    //     summary: this.editForm.summary,
-    //     body: this.editForm.body
-    //   }
-    //   this.$axios.post(path, payload)
-    //     .then((res) => {
-    //       this.getSearchResult()
-    //       this.$toasted.success('Successfully update this post', { icon: 'fingerprint' })
-    //       this.editForm.title = ''
-    //       this.editForm.summary = ''
-    //       this.editForm.body = ''
-    //     })
-    //     .catch((err) => {
-    //       for (const field in err.response.data.message) {
-    //         if (field === 'title') {
-    //           this.editForm.titleError = err.response.data.message[field]
-    //         } else if (field === 'body') {
-    //           this.editForm.bodyError = err.response.data.message[field]
-    //         }
-    //       }
-    //     })
-    // },
-    // onResetUpdatePost () {
-    //   this.$toasted.info('Cancelled, the post is not update.', { icon: 'fingerprint' })
-    // },
-    onDeletePost(post) {
-      this.$refs.confirm.confirm().then(res => {
-        const path = `/posts/${post.id}`;
-        this.$axios
-          .delete(path)
-          .then(res => {
-            this.$toasted.success('Delete successfully', { icon: 'fingerprint' });
-            this.getSearchResult();
-          })
-          .catch(err => {
-            console.log(err);
-          });
-      });
+        .catch(e => {
+          console.log(e);
+          this.$toasted.error(e.message);
+        })
     },
   },
   beforeRouteUpdate(to, from, next) {
@@ -213,22 +112,29 @@ export default {
 };
 </script>
 
-<style scoped>
-.result {
+<style lang="scss" scoped>
+$borderWidth: 3px;
+.result-post {
   width: 100%;
+  height: 200px;
+  margin-bottom: 20px;
   position: relative;
-  right: 0;
+  z-index: 2;
+  border-radius: $borderWidth;
+  .result-post-footer {
+    display: flex;
+    justify-content: flex-end;
+  }
 }
-.res-title {
-  font-size: 20px;
-  font-weight: bold;
-  padding-top: 20px;
-  border-bottom: 2px #928bad solid;
-}
-.res-body {
-}
-.res-other {
-  display: inline-flex;
-  float: right;
+.result-post:after {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: calc(-1 * #{$borderWidth} * 2);
+  height: 100%;
+  width: 1%;
+  background: linear-gradient(60deg, #f79533, #f37055, #ef4e7b, #a166ab, #5073b8, #1098ad, #07b39b, #6fba82);
+  z-index: -1;
 }
 </style>
+
