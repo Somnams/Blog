5ae8a19c1442b889064bec2e4d5e2bdaefff1d4a
Somnams <3244345132@qq.com>
2021-04-24 21:33:30 +0800

    ☁ add layout.vue

---
+++ front-end/src/components/common/Layout.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="page">
+    <nav-bar class="sidebar"/>
+    <div>
+      <header><nav-header/></header>
+      <div class="main-content">
+        <slot name="content"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import NavBar from '@components/common/nav/NavBar';
+import NavHeader from '@components/common/nav/NavHeader';
+export default {
+  name: 'Layout',
+  components: {
+    NavBar,
+    NavHeader
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
